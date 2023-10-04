0ef747aa6bfe922fc453638cbb4d5a98e9957d0e
Seedking <875828815@qq.com>
2023-10-05 02:21:40 +0800

    add packager main code file

---
+++ packager/src/packager.cpp
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2023.
+// This file is part of Ougan (https://github.com/Seedking/Ougan) project,
+// licensed under the MIT License (MIT).
+//
+// Created on 2023/9/4
+//
+
+#include "CLI/CLI.hpp"
+
+int main(int argc, char *argv[]) {
+    //
+    return 0;
+}
